54bb05103c4bf3503bb11e8c3894c394b61e1655
ryzfitness <104311100+ryzfitness@users.noreply.github.com>
2025-12-17 12:53:41 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.style.opacity = 1;
+});
+
+window.addEventListener("beforeunload", () => {
+  document.body.style.opacity = 0;
+});
 
