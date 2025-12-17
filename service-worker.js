b8d65b9eda5b5dbfad35cd42a71a75bf1a7a1f3f
ryzfitness <104311100+ryzfitness@users.noreply.github.com>
2025-12-17 12:54:20 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,22 @@
-const cacheName = 'ryz-v1';
-const assets = [
+const cacheName = 'ryz-cache-v1';
+const filesToCache = [
   '/',
   '/index.html',
-  '/style.css',
-  '/app.js'
+  '/about.html',
+  '/why-choose-ryz.html',
+  '/programs.html',
+  '/bible-study.html',
+  '/community.html',
+  '/plan.html',
+  '/contact.html',
+  '/css/style.css',
+  '/js/app.js'
 ];
 
-// Install event
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
 });
 
-// Activate event
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
-});
-
-// Fetch event
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
